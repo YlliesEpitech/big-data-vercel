ae3f67258302a4fb3f025d21cc1c03f9a75da6ca
YlliesEpitech <148759238+YlliesEpitech@users.noreply.github.com>
2024-09-26 17:00:46 +0200

    get html website

--- api/src/index.js
+++ api/src/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
+const cheerio = require("cheerio");
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hi" });
+app.get("/", async (req, res) => {
+  const fetchData = async () => {
+    const url = "https://www.coingecko.com/";
+    const response = await fetch(url);
+    const data = await response.text();
+    return data;
+  };
+  const data = await fetchData();
+
+  res.json({ data: data });
 });
 
 app.get("/hello", (req, res) => {
