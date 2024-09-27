b0c80512cf6dfce2d0c42481186a72e02eebe165
YlliesEpitech <148759238+YlliesEpitech@users.noreply.github.com>
2024-09-27 16:56:19 +0200

    get crypto names

--- api/src/index.js
+++ api/src/index.js
@@ -4,11 +4,27 @@ const cheerio = require("cheerio");
 
 app.get("/", async (req, res) => {
   const fetchData = async () => {
+    // Récupère l'html de la page
     const url = "https://www.coingecko.com/";
     const response = await fetch(url);
     const data = await response.text();
-    return data;
+
+    // Récupère les valeurs que je souhaite de la page.
+    getQuestions(data);
   };
+
+  // Fonction pour récuperer les valeurs que je souhaite
+  const getQuestions = async (html) => {
+    const $ = cheerio.load(html);
+    $(
+      ".tw-cursor-pointer.tw-relative.tw-inline-flex.tw-items-center.tw-rounded-lg.tw-px-4.tw-py-1\\.5.tw-text-sm.tw-font-semibold.\\!tw-text-gray-900.hover\\:tw-bg-gray-50.dark\\:\\!tw-text-moon-50.dark\\:hover\\:tw-bg-moon-700",
+
+      html
+    ).each(function () {
+      console.log($(this).text());
+    });
+  };
+
   const data = await fetchData();
 
   res.json({ data: data });
