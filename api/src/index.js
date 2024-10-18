270932795603ff13753c96ca66a877a930cbff51
YlliesEpitech <148759238+YlliesEpitech@users.noreply.github.com>
2024-10-18 11:27:27 +0200

    update vercel

--- api/src/index.js
+++ api/src/index.js
@@ -8,9 +8,57 @@ const { fetchData } = require('./utils/functions/AllExports');
 const app = express();
 
 
+const saveCryptoCurrencies = async () => {
+  try {
+    const data = await fetchData();
+
+    // Itérer sur chaque objet dans le tableau `data`
+    for (const crypto of data) {
+      // Vérifie si la cryptomonnaie existe déjà dans la db
+      let existingCrypto = await CryptoCurrency.findOne({ name: crypto.name, symbol: crypto.symbol });
+
+      // Créer une nouvelle devise
+      const newCurrency = new Currency({
+        currency: crypto.price,
+        date: new Date(),
+      });
+
+      await newCurrency.save(); // Attendre que la devise soit sauvegardée
+
+      if (existingCrypto) {
+        // Si elle existe, ajouter la nouvelle devise à l'array currencies de la cryptomonnaie existante
+        existingCrypto.currencies.push(newCurrency._id);
+        await existingCrypto.save(); // Enregistrer les changements
+      } else {
+        // Si elle n'existe pas, créer une nouvelle cryptomonnaie
+        const newCrypto = new CryptoCurrency({
+          name: crypto.name,
+          symbol: crypto.symbol,
+          currencies: [newCurrency._id],
+        });
+        await newCrypto.save(); // Sauvegarder la nouvelle cryptomonnaie
+      }
+    }
+    console.log("Cryptos ajoutées", data);
+  } catch (error) {
+    console.error("Erreur lors de la création des cryptos", error);
+  }
+};
+// Exécuter saveCryptoCurrencies toutes les 5 minutes
+setInterval(saveCryptoCurrencies, 5 * 60 * 1000);
 
 
-app.get("/", async (req, res) => {
+app.get("/cryptocurrencies", async (req, res) => {
+  try {
+    const cryptos = await CryptoCurrency.find().populate('currencies');
+    res.json(cryptos);
+  } catch (error) {
+    console.error("Erreur lors de la récupération des cryptomonnaies", error);
+    res.status(500).json({ error: "Erreur lors de la récupération des cryptomonnaies" });
+  }
+});
+
+app.post("/create", async (req, res) => {
   try {
     const data = await fetchData();
 
@@ -49,47 +97,6 @@ app.get("/", async (req, res) => {
   }
 });
 
-
-const saveCryptoCurrencies = async () => {
-  try {
-    const data = await fetchData();
-
-    // Itérer sur chaque objet dans le tableau `data`
-    for (const crypto of data) {
-      // Vérifie si la cryptomonnaie existe déjà dans la db
-      let existingCrypto = await CryptoCurrency.findOne({ name: crypto.name, symbol: crypto.symbol });
-
-      // Créer une nouvelle devise
-      const newCurrency = new Currency({
-        currency: crypto.price,
-        date: new Date(),
-      });
-
-      await newCurrency.save(); // Attendre que la devise soit sauvegardée
-
-      if (existingCrypto) {
-        // Si elle existe, ajouter la nouvelle devise à l'array currencies de la cryptomonnaie existante
-        existingCrypto.currencies.push(newCurrency._id);
-        await existingCrypto.save(); // Enregistrer les changements
-      } else {
-        // Si elle n'existe pas, créer une nouvelle cryptomonnaie
-        const newCrypto = new CryptoCurrency({
-          name: crypto.name,
-          symbol: crypto.symbol,
-          currencies: [newCurrency._id],
-        });
-        await newCrypto.save(); // Sauvegarder la nouvelle cryptomonnaie
-      }
-    }
-    console.log("Cryptos ajoutées", data);
-  } catch (error) {
-    console.error("Erreur lors de la création des cryptos", error);
-  }
-};
-
-
-// Exécuter saveCryptoCurrencies toutes les 5 minutes
-setInterval(saveCryptoCurrencies, 5 * 60 * 1000);
 
 
 
