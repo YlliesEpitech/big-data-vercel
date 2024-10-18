743260859905a437ed635db57fae71fa3bc81d88
YlliesEpitech <148759238+YlliesEpitech@users.noreply.github.com>
2024-10-18 15:45:21 +0200

    add cron

--- api/app.js
+++ api/app.js
@@ -11,46 +11,6 @@ const app = express();
 
 app.use(cors());
 
-const saveCryptoCurrencies = async () => {
-  try {
-    const data = await fetchData();
-
-    // Itérer sur chaque objet dans le tableau `data`
-    for (const crypto of data) {
-      // Vérifie si la cryptomonnaie existe déjà dans la db
-      let existingCrypto = await CryptoCurrency.findOne({ name: crypto.name, symbol: crypto.symbol });
-
-      // Créer une nouvelle devise
-      const newCurrency = new Currency({
-        currency: crypto.price,
-        date: new Date(),
-      });
-
-      await newCurrency.save(); // Attendre que la devise soit sauvegardée
-
-      if (existingCrypto) {
-        // Si elle existe, ajouter la nouvelle devise à l'array currencies de la cryptomonnaie existante
-        existingCrypto.currencies.push(newCurrency._id);
-        await existingCrypto.save(); // Enregistrer les changements
-      } else {
-        // Si elle n'existe pas, créer une nouvelle cryptomonnaie
-        const newCrypto = new CryptoCurrency({
-          name: crypto.name,
-          symbol: crypto.symbol,
-          currencies: [newCurrency._id],
-        });
-        await newCrypto.save(); // Sauvegarder la nouvelle cryptomonnaie
-      }
-    }
-    console.log("Cryptos ajoutées", data);
-  } catch (error) {
-    console.error("Erreur lors de la création des cryptos", error);
-  }
-};
-// Exécuter saveCryptoCurrencies toutes les 5 minutes
-setInterval(saveCryptoCurrencies, 5 * 60 * 1000);
-
-
 app.get("/cryptocurrencies", async (req, res) => {
   try {
     const cryptos = await CryptoCurrency.find().populate('currencies');
